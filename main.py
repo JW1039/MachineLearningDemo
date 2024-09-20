30dcbede777ccc764239888e5a12c1ea401057ec
Jeremy <103984916@student.swin.edu.au>
2024-09-20 21:31:17 +1000

    basic changes

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 from tensorflow.keras.models import Sequential
+
+data = pd.read_csv("data/purchase_data.csv")
+print(data)
+model = Sequential()
